dba534ef355d0ee4f8732efe4cc3a4003bb60d7b
40620 <40620@PC004138.coll.lr.local>
2014-10-14 15:16:29 +0100

    do this

---
+++ Iteration tast 1.py
@@ -0,0 +1,14 @@
+#Danish ALi
+#iteration stater
+#Task 1
+#13-oct-2014
+
+
+total = 0
+counter = 0
+
+while counter < 5:
+    number = int(input("Please enter number {0} in the series: ".format(counter)))
+    total = total + number
+    counter = counter + 1
+print("The total of the numbers you entered is {0}: ".format(total))
